f6b6c33393dccc527e1e552ca6ad5fa8dafaa034
mbeddeveloper <franz.raumschuessel@stud.th-deg.de>
2018-08-13 14:21:08 +0200

    graph working

--- maschine_learning.py
+++ maschine_learning.py
@@ -41,8 +41,13 @@ y = np.array(df['label'])
 
 X_train, X_test, y_train, y_test = cross_validation.train_test_split(X, y, test_size=0.2)
 
-clf = LinearRegression(n_jobs=-1)
-clf.fit(X_train, y_train)
+#clf = LinearRegression(n_jobs=-1)
+#clf.fit(X_train, y_train)
+#with open('linearregression.pickle', 'wb') as f:
+#    pickle.dump(clf, f)
+
+pickle_in = open ('linearregression.pickle', 'rb')
+clf = pickle.load(pickle_in)
 accuaracy = clf.score(X_test, y_test)
 forecast_set = clf.predict(X_lately)
 print(forecast_set, accuaracy, forecast_out)
